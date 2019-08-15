58d6476b1898d12d2073dfb776eee9b18b19e3f8
arx-8 <swans.randomness@gmail.com>
2019-08-15 15:35:41 +0900

    wip

--- src/components/pages/Root/index.tsx
+++ src/components/pages/Root/index.tsx
@@ -1,18 +1,48 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
 import logo from "assets/logo.svg"
-import React from "react"
+import React, { useState } from "react"
+import html2canvas from "html2canvas"
 
 type OwnProps = {
   children?: never
 }
 
 export const Root: React.FC<OwnProps> = () => {
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
+
+  const onClick = async (): Promise<void> => {
+    const canvas = await html2canvas(document.body, {
+      allowTaint: true,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      scrollX: window.pageXOffset,
+      scrollY: window.pageYOffset,
+      x: window.pageXOffset,
+      y: window.pageYOffset,
+    })
+
+    canvas.style.position = "fixed"
+    canvas.style.top = "0"
+    canvas.style.left = "0"
+    canvas.style.opacity = "0"
+    canvas.style.transform = "scale(0)"
+    canvas.style.zIndex = "99999999"
+    canvas.style.transition =
+      "transform 0.3s cubic-bezier(0.42, 0, 0.58, 1),opacity 0.3s cubic-bezier(0.42, 0, 0.58, 1),-webkit-transform 0.3s cubic-bezier(0.42, 0, 0.58, 1)"
+
+    setCanvas(canvas)
+  }
+
   return (
     <div css={root}>
       <header css={header}>
         <img css={logoCss} src={logo} alt="logo" />
         <p>Hello React</p>
+        <button onClick={onClick}>Capture</button>
+        {canvas && (
+          <CanvasContainer canvas={canvas} onClose={() => setCanvas(null)} />
+        )}
         <a
           css={link}
           href="https://reactjs.org"
@@ -42,20 +72,64 @@ const header = css`
 `
 
 const logoCss = css`
-  animation: App-logo-spin infinite 20s linear;
   height: 40vmin;
-  pointer-events: none;
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
 `
 
 const link = css`
   color: #61dafb;
+`
+
+type InnerOwnProps = {
+  canvas: HTMLCanvasElement
+  onClose: () => void
+}
+
+class CanvasContainer extends React.Component<InnerOwnProps> {
+  private container: HTMLDivElement | null = null
+
+  constructor(props: InnerOwnProps) {
+    super(props)
+    this.state = { open: false, complete: false }
+  }
+
+  componentDidMount(): void {
+    if (this.container) {
+      this.container.appendChild(this.props.canvas)
+      setTimeout(() => {
+        this.props.canvas.style.opacity = "1"
+        this.props.canvas.style.transform = "scale(0.8)"
+      }, 10)
+    }
+  }
+
+  render(): React.ReactElement {
+    return (
+      <div
+        css={canvasContainerCss}
+        ref={(container) => (this.container = container)}
+      >
+        <button
+          onClick={() => this.props.onClose()}
+          style={{
+            position: "absolute",
+            right: "20px",
+            top: "20px",
+            cursor: "pointer",
+          }}
+        >
+          Close
+        </button>
+      </div>
+    )
+  }
+}
+
+const canvasContainerCss = css`
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 99999998;
+  width: 100%;
+  height: 100%;
 `
